bc8e6373f0c760016327c953abeb7e262b7ec3a7
lychen <412369718@qq.com>
2021-08-12 16:02:45 +0800

    add head sort

---
+++ include/chapter06/algorithm.hpp
@@ -0,0 +1,66 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+namespace chapter06
+{
+    struct Heap
+    {
+        /* data */
+        std::vector<int>& array;
+        size_t heap_size;
+
+        Heap(std::vector<int>& _array) : array(_array), heap_size(_array.size() - 1) {}
+    };
+
+    inline size_t get_parent(size_t i){
+        return (i - 1) / 2;
+    }
+
+    inline size_t left_child(size_t i)
+    {
+        return 2 * i + 1;
+    }
+
+    inline size_t right_child(size_t i)
+    {
+        return 2 * (i + 1);
+    }
+
+    void max_heapify(Heap heap, size_t i)
+    {
+        size_t larget_index = i;
+        size_t left_child_index = left_child(i);
+        size_t right_child_index = right_child(i);
+        if (left_child_index <= heap.heap_size &&
+            heap.array[left_child_index] > heap.array[larget_index])
+            larget_index = left_child_index;
+        if (right_child_index <= heap.heap_size &&
+            heap.array[right_child_index] > heap.array[larget_index])
+            larget_index = right_child_index;
+        if (larget_index != i)
+        {
+            std::swap(heap.array[i], heap.array[larget_index]);
+            max_heapify(heap, larget_index);
+        }
+    }
+
+    void build_max_heap(Heap heap)
+    {
+        for(size_t i = get_parent(heap.heap_size); i >= 0; --i){
+            max_heapify(heap, i);
+            if(i == 0) break;
+        }
+    }
+
+    void heap_sort(std::vector<int>& array){
+        Heap heap(array);
+        build_max_heap(heap);
+        for(int i = heap.heap_size; i > 0; --i){
+            std::swap(heap.array[0], heap.array[i]);
+            heap.heap_size -= 1;
+            max_heapify(heap, 0);
+        }
+    }
+}
--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "chapter09/algorithm.hpp"
 #include "chapter07/algorithm.hpp"
+#include "chapter06/algorithm.hpp"
 #include "chapter02/algorithm.hpp"
 using namespace std;
 
@@ -20,7 +21,8 @@ void test_get_max_and_min()
     assert((*min_value_pointer) == 1);
 }
 
-void test_select_method(){
+void test_select_method()
+{
     vector<int> array1{1, 5, 3, 7, 8, 6};
     vector<int> array1_copy = array1;
     assert(chapter09::expect_linear_time_select(array1_copy, 3) == 5);
@@ -76,6 +78,27 @@ void test_sort_method()
     {
         assert(array1_copy[i - 1] <= array1_copy[i]);
     }
+
+    array1_copy = array1;
+    chapter06::heap_sort(array1_copy);
+    for (size_t i = 1; i < array1_copy.size(); ++i)
+    {
+        assert(array1_copy[i - 1] <= array1_copy[i]);
+    }
+
+    array1_copy = array2;
+    chapter06::heap_sort(array1_copy);
+    for (size_t i = 1; i < array1_copy.size(); ++i)
+    {
+        assert(array1_copy[i - 1] <= array1_copy[i]);
+    }
+}
+
+void test_util_random()
+{
+    for (int i = 0; i < 10; ++i)
+        cout << util::randint(1, 2) << " ";
+    cout << endl;
 }
 
 int main()
@@ -83,5 +106,6 @@ int main()
     test_get_max_and_min();
     test_select_method();
     test_sort_method();
+    test_util_random();
     return 0;
 }
